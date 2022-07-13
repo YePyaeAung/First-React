4778918a2ec9e9343d910258f79f2fe6fb2361ea
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-07-13 12:15:19 +0630

    React List and Keys

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     super();
     this.state = {
       movies: [
-        { name: "Spider Man 1", year: 2009 },
-        { name: "Spider Man 2", year: 2011 },
-        { name: "Spider Man 3", year: 2015 },
+        { id: 1, name: "Spider Man 1", year: 2009 },
+        { id: 2, name: "Spider Man 2", year: 2011 },
+        { id: 3, name: "Spider Man 3", year: 2015 },
       ],
     };
   }
@@ -26,9 +26,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello World...</h1>
-        <p>My Favourite Movie is { this.state.movies[0].name }. Released Year { this.state.movies[0].year }</p>
-        <p>My Favourite Movie is { this.state.movies[1].name }. Released Year { this.state.movies[1].year }</p>
-        <p>My Favourite Movie is { this.state.movies[2].name }. Released Year { this.state.movies[2].year }</p>
+        { this.state.movies.map((movie) => {
+          return <h3 key={movie.id}>
+            My Favourite Movie is { movie.name }. Released Year is { movie.year }</h3>
+        })}
         <input type="text" onChange={ this.changeName } value={ this.state.movies[0].name }/>
       </div>
     );
