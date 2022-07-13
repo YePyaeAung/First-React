70d3734b194139c52832cb2908c511a0ba745f80
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-07-13 12:41:15 +0630

    Get Movies From API

--- src/App.js
+++ src/App.js
@@ -6,31 +6,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { id: 1, name: "Spider Man 1", year: 2009 },
-        { id: 2, name: "Spider Man 2", year: 2011 },
-        { id: 3, name: "Spider Man 3", year: 2015 },
-      ],
+      movies: [],
     };
   }
-  changeName = (event) => {
-    this.setState({
-      movies: [
-        { name: event.target.value, year: 2018 },
-        { name: "Spider Man 2", year: 2011 },
-        { name: "Spider Man 3", year: 2015 },
-      ],
-    });
+  componentDidMount() {
+    fetch("https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies")
+    .then((response) => response.json())
+    .then((apiMovies) => this.setState(() => {
+      return { movies: apiMovies };
+    }));
   }
   render() {
     return (
       <div className="App">
         <h1>Hello World...</h1>
         { this.state.movies.map((movie) => {
-          return <h3 key={movie.id}>
-            My Favourite Movie is { movie.name }. Released Year is { movie.year }</h3>
+          return <h3 key={movie.Title}>
+            My Favourite Movie is { movie.Title }. Released Year is { movie.Year }</h3>
         })}
-        <input type="text" onChange={ this.changeName } value={ this.state.movies[0].name }/>
       </div>
     );
   }
