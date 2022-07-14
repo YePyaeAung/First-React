083779c03531b6cbcc83f10da90c1d4b4a0b1097
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-07-14 08:01:48 +0630

    Create Movie Card Component

--- src/Components/movie-list/movie-list.jsx
+++ src/Components/movie-list/movie-list.jsx
@@ -1,12 +1,12 @@
 import { Component } from "react";
+import MovieCard from "../movie-card/movie-card";
 
 class MovieList extends Component {
     render() {
         return (
             <div>
                 { this.props.movies.map((movie) => {
-                    return <h3 key={movie.Title}>
-                    My Favourite Movie is { movie.Title }. Released Year is { movie.Year }</h3>
+                    return <MovieCard movie={ movie }/>
                 })}
             </div>
             );
