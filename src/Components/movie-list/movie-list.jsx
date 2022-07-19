0a111aa0ddd27d34e2062a04078219e2b53ad5dd
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-07-19 20:17:33 +0630

    Convert App and MovieList to Function Component

--- src/Components/movie-list/movie-list.jsx
+++ src/Components/movie-list/movie-list.jsx
@@ -1,16 +1,28 @@
-import { Component } from "react";
-import MovieCard from "../movie-card/movie-card";
+import MovieCard from '../movie-card/movie-card';
 
-class MovieList extends Component {
-    render() {
-        return (
-            <div className="grid grid-cols-3 gap-4">
-                { this.props.movies.map((movie) => {
-                    return <MovieCard movie={ movie }/>
-                })}
-            </div>
-            );
-        }
-    }
-    
-    export default MovieList;
+const MovieList = ({movies}) => {
+    return (
+        <div className="grid grid-cols-3 gap-4">
+            { movies.map((movie) => {
+                return <MovieCard movie={ movie }/>
+            })}
+        </div>
+    )
+}
+export default MovieList;
+
+// import { Component } from "react";
+// import MovieCard from "../movie-card/movie-card";
+
+// class MovieList extends Component {
+//     render() {
+//         return (
+//             <div className="grid grid-cols-3 gap-4">
+//                 { this.props.movies.map((movie) => {
+//                     return <MovieCard movie={ movie }/>
+//                 })}
+//             </div>
+//             );
+//         }
+//     }
+// export default MovieList;
